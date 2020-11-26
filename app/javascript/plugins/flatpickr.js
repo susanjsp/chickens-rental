0cc9aa5a4181c4d4cb118ce573a01d7657e3ec8a
Susan Pan <susan.pan96@gmail.com>
2020-11-26 17:17:36 +1000

    FEAT: updated datepickr theme and added range selector

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,16 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr(".datepicker", {
+// flatpickr(".datepicker", {
+//   altInput: true,
+//   allowInput: true
+// });
+
+flatpickr("#booking_start_date", {
   altInput: true,
-  allowInput: true
+  allowInput: true,
+  dateFormat: "d-m-Y",
+  defaultDate: "today",
+  minDate: "today",
+  plugins: [new rangePlugin({ input: "#booking_end_date"})]
 });
