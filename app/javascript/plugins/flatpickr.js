3bc8f8c3a9783bcc7c34b2c720c19ce5e9d28cd4
Susan Pan <susan.pan96@gmail.com>
2020-11-26 17:57:06 +1000

    UPDATE: tried to fix datepickr - it's a bit buggy

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -5,12 +5,26 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 //   altInput: true,
 //   allowInput: true
 // });
+const endDate = document.querySelector('#booking_end_date');
 
 flatpickr("#booking_start_date", {
   altInput: true,
   allowInput: true,
-  dateFormat: "d-m-Y",
-  defaultDate: "today",
+  // mode: "range",
+  // dateFormat: "d.m.Y",
   minDate: "today",
-  plugins: [new rangePlugin({ input: "#booking_end_date"})]
+  plugins: [new rangePlugin({ input: "#booking_end_date"})],
+
+  onChange: function(selectedDates, selectedDate) {
+    if (selectedDate === '') {
+      endDate.disabled = true;
+    } else if (selectedDate > endDate) {
+      selectedDates = [];
+    }
+
+    let minDate = selectedDates[0];
+    minDate.setDate(minDate.getDate() + 1);
+    endDate.disabled = false;
+  },
 });
+
