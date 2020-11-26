853b4ae17f126bb2f77132898980509f8412715d
Susan Pan <susan.pan96@gmail.com>
2020-11-26 22:30:09 +1000

    FIX: datepickr range too buggy, switched back to regular calendar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,30 +1,20 @@
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-// flatpickr(".datepicker", {
-//   altInput: true,
-//   allowInput: true
-// });
-const endDate = document.querySelector('#booking_end_date');
+const end_date = document.querySelector('#booking_end_date');
 
-flatpickr("#booking_start_date", {
+flatpickr(".datepicker", {
   altInput: true,
-  allowInput: true,
-  // mode: "range",
-  // dateFormat: "d.m.Y",
   minDate: "today",
-  plugins: [new rangePlugin({ input: "#booking_end_date"})],
-
-  onChange: function(selectedDates, selectedDate) {
-    if (selectedDate === '') {
-      endDate.disabled = true;
-    } else if (selectedDate > endDate) {
-      selectedDates = [];
-    }
-
-    let minDate = selectedDates[0];
-    minDate.setDate(minDate.getDate() + 1);
-    endDate.disabled = false;
-  },
 });
 
+// flatpickr(end_date, {
+//   minDate: end_date.value + 1;
+// });
+// flatpickr("#booking_start_date", {
+//   altInput: true,
+//   allowInput: true,
+//   // mode: "range",
+//   minDate: 'today',
+//   plugins: [new rangePlugin({ input: "#booking_end_date"})],
+// });
